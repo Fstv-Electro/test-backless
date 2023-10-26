b5d3af39ef63429e214682cbcdcc00ff896acabf
Oleksandr Kutsenko <104622826+Fstv-Electro@users.noreply.github.com>
2023-10-26 14:54:37 +0300

    fix: fix name and url

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/test-backendless/">
+    <BrowserRouter basename="/test-backless/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
